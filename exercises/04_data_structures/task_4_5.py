2d029493fbc98eca2ffbcab32d371daf68e5e260
vitalishevtsov <vitali.shevtsov@gmail.com>
2022-02-13 20:44:01 +0000

    Выполнено задание 4.5

--- exercises/04_data_structures/task_4_5.py
+++ exercises/04_data_structures/task_4_5.py
@@ -22,3 +22,8 @@
 
 command1 = "switchport trunk allowed vlan 1,2,3,5,8"
 command2 = "switchport trunk allowed vlan 1,3,8,9"
+
+command1 = command1.split()[-1].split(',')
+command2 = command2.split()[-1].split(',')
+result = sorted(set(command1).intersection(set(command2)))
+print(result)
