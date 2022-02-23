c173ff39db7d1205f9e6654b2df09163f6cfb477
vitalishevtsov <vitali.shevtsov@gmail.com>
2022-02-23 21:57:39 +0000

    Задание 6_2b

--- exercises/06_control_structures/task_6_2b.py
+++ exercises/06_control_structures/task_6_2b.py
@@ -12,3 +12,39 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+
+correct_ip = False
+
+while not correct_ip:
+    ip_address = input("Enter ip address: ")
+    octets = ip_address.split(".")
+    correct_ip = True
+
+    if len(octets) != 4:
+        correct_ip = False
+    else:
+        for octet in octets:
+        # тут второе условие int(octet) in range(256)
+        # проверяется только в том случае, если первое условие истина
+        # Если встретился хоть один октет с нарушением,
+        # дальше можно не смотреть
+            if not (octet.isdigit() and int(octet) in range(256)):
+                correct_ip = False
+                break
+
+    if not correct_ip:
+        print("неправильный ip-адрес")
+    else:
+        octets_num = [int(i) for i in octets]
+
+        if octets_num[0] in range(1, 224):
+            print("unicast")
+        elif octets_num[0] in range(224, 240):
+            print("multicast")
+        elif ip_address == "255.255.255.255":
+            print("local broadcast")
+        elif ip_address == "0.0.0.0":
+            print("unassigned")
+        else:
+            print("unused")
+
