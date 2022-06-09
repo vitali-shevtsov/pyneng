a24bf399ae7f73f0f06b9137796755a669974fb9
vitalishevtsov <vitali.shevtsov@gmail.com>
2022-06-09 14:41:26 +0000

    Решены задания 12_1

--- exercises/12_useful_modules/task_12_1.py
+++ exercises/12_useful_modules/task_12_1.py
@@ -17,3 +17,24 @@ IP-адрес считается доступным, если выполнени
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+
+import subprocess
+
+#result = subprocess.run(['ping', '-c', '3', '-n', '8.8.8.8'], stdout=subprocess.PIPE)
+#result = subprocess.run(['ping', '-c', '3', '-n', '8.8.8.8'], stdout=subprocess.DEVNULL)
+#print(result.stdout.decode('utf-8'))
+#print(result.stdout)
+#print(result.returncode)
+
+def ping_ip_addresses(ip_addr_list):
+    available_ip = []
+    unavailable_ip = []
+    for i in ip_addr_list:
+        result = subprocess.run(['ping', '-c', '3', '-n', i], stdout=subprocess.DEVNULL)
+        if result.returncode == 0:
+            available_ip.append(i)
+        else:
+            unavailable_ip.append(i)
+    return(available_ip, unavailable_ip)
+
+print(ping_ip_addresses(['8.8.8.8','77.88.8.8','93.125.15.1']))
